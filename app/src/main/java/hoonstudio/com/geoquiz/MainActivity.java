7b3fb1db200795d526b952c26e39a9c7aa0bfb3f
Bryan Lee <bryanlee21@live.com>
2018-04-27 16:42:58 -0700

    Updated Android Studio

--- app/src/main/java/hoonstudio/com/geoquiz/MainActivity.java
+++ app/src/main/java/hoonstudio/com/geoquiz/MainActivity.java
@@ -2,12 +2,15 @@ package hoonstudio.com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     //Buttons
     private Button mTrueButton;
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: called");
         setContentView(R.layout.activity_main);
 
         mTrueButton = (Button) findViewById(R.id.trueButton);
@@ -87,6 +91,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         updateQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
     }
 
     public void updateQuestion(){
